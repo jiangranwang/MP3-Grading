68d7deb76d21c994aff62c211fa53118568f5845
jiangranwang <wangjiangran3@gmail.com>
2021-12-19 19:46:09 +0800

    update isolation tests

--- README.md
+++ README.md
@@ -1,56 +1,57 @@
 ### Atomicity Tests
-a1.txt: abort pre-exisiting account
+a1.txt: abort pre-exisiting account (8 points)
 
-a2.txt: abort newly created account
+a2.txt: abort newly created account (8 points)
 
-a3.txt: ignore commands outside transactions
+a3.txt: ignore commands outside transactions (4 points)
 
 ### Consistency Tests
-c1.txt: transaction involving multiple servers
+c1.txt: transaction involving multiple servers (8 points)
 
-c2.txt: negative balance during transaction but eventually resolved
+c2.txt: negative balance during transaction but eventually resolved (6 points)
 
-c3.txt: commit with negative balance
+c3.txt: commit with negative balance (6 points)
 
 ### Isolation Tests
 Each test runs 5 times
 
-#### 10 clients write to different accounts within time limit
+#### 10 clients write to different accounts within time limit (5 points)
 i1.txt: first only 1 client write to 1 account 10 times and get how long it takes as T
 
 i2.txt - i-11.txt: each client write to different accounts 10 times and limit execution time within 2T
 
 i12.txt: check final balance of all accounts with i12-expected.txt
 
-#### N clients write once to single account
+#### N clients write twice to single account (2, 3, 5 points for N=2, 5, 10)
 iN-1.txt: initialize account x
 
-iN-2.txt: deposit 10 x
+iN-2.txt: deposit 10 x, withdraw 5 x
 
 iN-3.txt: display balance of account x
 
-Compare the output of iN-3.txt with iN-3-expected.txt (final value should be 10N)
+Compare the output of iN-3.txt with iN-3-expected.txt (final value should be 5N)
 
 N={2,5,10}, run in the order of iN-1.txt -> NxiN-2.txt -> iN-3.txt
 
-#### N clients read/write multiple times to single account
+#### N clients read/write multiple times to single account (2, 3, 5 points for N=2, 5, 10)
 iN-4.txt: initialize account x
 
-iN-5.txt: deposit 10 x, balance x, deposit 10 x, balance x, deposit 10 x, balance x (the output of three balance commands should each have a difference of 10, i.e. y, y+10, y+20)
+iN-5.txt: deposit 10 x, balance x, deposit 10 x, balance x... (repeat 10 times, the output of ten balance commands should each have a difference of 10, i.e. y, y+10, y+20...)
 
 Compare the output of iN-5.txt with iN-5-expected.txt
 
 iN-6.txt: display balance of account x
 
-Compare the output of iN-6.txt with iN-6-expected.txt (Final value should be 30N)
+Compare the output of iN-6.txt with iN-6-expected.txt (Final value should be 100N)
 
 N={2,5,10}, run in the order of iN-4.txt -> NxiN-5.txt -> iN-6.txt
 
-#### N clients abort test
+#### N clients abort test (2, 3, 5 points for N=2, 5, 10)
 half clients abort? the other half clients' operation should be unaffected. Not sure how to design the tests
 
 ### Deadlock Tests
 Each test runs 5 times
+Each deposit operation is repeated 5 times to maximize possibiltiy of deadlock
 
 2 clients: client 1 (deposit a, deposite b), client 2 (deposit b, deposite a)
 
--- run_tests.sh
+++ run_tests.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 clients=(a b c d e f g h i j k l m n)
-folder='../g2'
+group_folder='../g2'
+curr_folder='../tests'
 final_score=0
-cp config.txt ${folder}/
+cp config.txt ${group_folder}/
 touch result.txt
 
 # Atomicity Test 1 (8)
 atest1 () {
-	cd $folder
+	cd ${group_folder}
 	score=8
 
 	./client a config.txt < ../tests/atomicity/a1.txt > ../a1.log
+	sed -i '' -e '$ d' ../a1.log
 	diff ../a1.log ../tests/atomicity/a1-expected.txt
 
 	read -p "Atomicity 1 Test Passed? (y/n)" yn
@@ -19,16 +21,17 @@ atest1 () {
 		[Nn]* ) echo 'Test Failed' >> ../result.txt;;
 	    * ) final_score=$(( $final_score+$score )); echo 'Test Passed' >> ../result.txt;;
 	esac
-	cd ..
+	cd ${curr_folder}
 	echo; echo;
 }
 
 # Atomicity Test 2 (8)
 atest2 () {
-	cd $folder
+	cd ${group_folder}
 	score=8
 
 	./client a config.txt < ../tests/atomicity/a2.txt > ../a2.log
+	sed -i '' -e '$ d' ../a2.log
 	diff ../a2.log ../tests/atomicity/a2-expected.txt
 
 	read -p "Atomicity 2 Test Passed? (y/n)" yn
@@ -37,16 +40,17 @@ atest2 () {
 		[Nn]* ) echo 'Test Failed' >> ../result.txt;;
 	    * ) final_score=$(( $final_score+$score )); echo 'Test Passed' >> ../result.txt;;
 	esac
-	cd ..
+	cd ${curr_folder}
 	echo; echo;
 }
 
 # Atomicity Test 3 (4)
 atest3 () {
-	cd $folder
+	cd ${group_folder}
 	score=4
 
 	./client a config.txt < ../tests/atomicity/a3.txt > ../a3.log
+	sed -i '' -e '$ d' ../a3.log
 	diff ../a3.log ../tests/atomicity/a3-expected.txt
 
 	read -p "Atomicity 3 Test Passed? (y/n)" yn
@@ -55,16 +59,17 @@ atest3 () {
 		[Nn]* ) echo 'Test Failed' >> ../result.txt;;
 	    * ) final_score=$(( $final_score+$score )); echo 'Test Passed' >> ../result.txt;;
 	esac
-	cd ..
+	cd ${curr_folder}
 	echo; echo;
 }
 
 # Consistency Test 1 (8)
 ctest1 () {
-	cd $folder
+	cd ${group_folder}
 	score=8
 
 	./client a config.txt < ../tests/consistency/c1.txt > ../c1.log
+	sed -i '' -e '$ d' ../c1.log
 	diff ../c1.log ../tests/consistency/c1-expected.txt
 
 	read -p "Consistency 1 Test Passed? (y/n)" yn
@@ -73,16 +78,17 @@ ctest1 () {
 		[Nn]* ) echo 'Test Failed' >> ../result.txt;;
 	    * ) final_score=$(( $final_score+$score )); echo 'Test Passed' >> ../result.txt;;
 	esac
-	cd ..
+	cd ${curr_folder}
 	echo; echo;
 }
 
 # Consistency Test 2 (6)
 ctest2 () {
-	cd $folder
+	cd ${group_folder}
 	score=6
 
 	./client a config.txt < ../tests/consistency/c2.txt > ../c2.log
+	sed -i '' -e '$ d' ../c2.log
 	diff ../c2.log ../tests/consistency/c2-expected.txt
 
 	read -p "Consistency 2 Test Passed? (y/n)" yn
@@ -91,16 +97,17 @@ ctest2 () {
 		[Nn]* ) echo 'Test Failed' >> ../result.txt;;
 	    * ) final_score=$(( $final_score+$score )); echo 'Test Passed' >> ../result.txt;;
 	esac
-	cd ..
+	cd ${curr_folder}
 	echo; echo;
 }
 
 # Consistency Test 3 (6)
 ctest3 () {
-	cd $folder
+	cd ${group_folder}
 	score=6
 
 	./client a config.txt < ../tests/consistency/c3.txt > ../c3.log
+	sed -i '' -e '$ d' ../c3.log
 	diff ../c3.log ../tests/consistency/c3-expected.txt
 
 	read -p "Consistency 3 Test Passed? (y/n)" yn
@@ -109,13 +116,13 @@ ctest3 () {
 		[Nn]* ) echo 'Test Failed' >> ../result.txt;;
 	    * ) final_score=$(( $final_score+$score )); echo 'Test Passed' >> ../result.txt;;
 	esac
-	cd ..
+	cd ${curr_folder}
 	echo; echo;
 }
 
 # Time Limit Test (5)
 tlimit () {
-	cd $folder
+	cd ${group_folder}
 	score=5
 	while true; do
 		TIME=($( { time ./client a config.txt < ../tests/isolation/i1.txt > /dev/null; } 2>&1 ))
@@ -139,13 +146,13 @@ tlimit () {
 		[Nn]* ) echo 'Test Failed' >> ../result.txt;;
 	    * ) final_score=$(( $final_score+$score )); echo 'Test Passed' >> ../result.txt;;
 	esac
-	cd ..
+	cd ${curr_folder}
 	echo; echo;
 }
 
 # Isolation Test 1 (2,3,5)
 itest1 () {
-	cd $folder
+	cd ${group_folder}
 	if [[ $1 -eq 2 ]]; then
 		score=2
 	elif [[ $1 -eq 5 ]]; then
@@ -159,44 +166,97 @@ itest1 () {
 		echo 'run num: '$i
 		./client a config.txt < ../tests/isolation/test1/i$1-1-$i.txt > /dev/null
 		for (( j=0; j<$1; j++ )); do
-			./client ${clients[$j]} config.txt < ../tests/isolation/test1/i$1-2-$i.txt > ../i1-$1-$i-$j.log
+			./client ${clients[$j]} config.txt < ../tests/isolation/test1/i$1-2-$i.txt > ../i1-$1-$i-$j.log &
 		done
 		./client a config.txt < ../tests/isolation/test1/i$1-3-$i.txt > ../i1-$1-$i.log
+		sed -i '' -e '$ d' ../i1-$1-$i.log
 		diff ../i1-$1-$i.log ../tests/isolation/test1/i$1-3-expected-$i.txt
 		echo
 	done
 
 	read -p "Isolation 1 Test Number of Runs Passed (out of "$num_run")? " n
-	echo 'Consistency Test 3: ' >> ../result.txt
+	echo 'Isolation Test 1 with '$1' clients: ' >> ../result.txt
 	echo $n'/'$num_run' Runs Passed' >> ../result.txt
 	curr_score=$( bc <<< 'scale=2; '$n'/'$num_run'*'$score )
 	echo 'score is: '$curr_score/$score'.00'
 	final_score=$( bc <<< 'scale=2; '$final_score'+'$curr_score )
-	cd ..
+	cd ${curr_folder}
+	echo; echo;
+}
+
+# Isolation Test 2 (2,3,5)
+itest2 () {
+	cd ${group_folder}
+	if [[ $1 -eq 2 ]]; then
+		score=2
+	elif [[ $1 -eq 5 ]]; then
+		score=3
+	elif [[ $1 -eq 10 ]]; then
+		score=5
+	fi
+	num_run=5
+
+	for (( i=0; i<$num_run; i++ )); do
+		echo 'run num: '$i
+		./client a config.txt < ../tests/isolation/test2/i$1-4-$i.txt > /dev/null
+		for (( j=0; j<$1; j++ )); do
+			./client ${clients[$j]} config.txt < ../tests/isolation/test2/i$1-5-$i.txt > ../i2-$1-$i-$j.log &
+		done
+		python3 ../tests/isolation/test2/check.py $j ../i2-$1-$i
+		./client a config.txt < ../tests/isolation/test2/i$1-6-$i.txt > ../i2-$1-$i.log
+		sed -i '' -e '$ d' ../i2-$1-$i.log
+		diff ../i2-$1-$i.log ../tests/isolation/test2/i$1-6-expected-$i.txt
+		echo
+	done
+
+	read -p "Isolation 2 Test Number of Runs Passed (out of "$num_run")? " n
+	echo 'Isolation Test 2 with '$1' clients: ' >> ../result.txt
+	echo $n'/'$num_run' Runs Passed' >> ../result.txt
+	curr_score=$( bc <<< 'scale=2; '$n'/'$num_run'*'$score )
+	echo 'score is: '$curr_score/$score'.00'
+	final_score=$( bc <<< 'scale=2; '$final_score'+'$curr_score )
+	cd ${curr_folder}
 	echo; echo;
 }
 
 # Atomicity Tests
-atest1
-atest2
-atest3
+# atest1
+# atest2
+# atest3
 
-# Consistency Tests
-ctest1
-ctest2
-ctest3
+# # Consistency Tests
+# ctest1
+# ctest2
+# ctest3
 
-# Isolation Test
-tlimit
-itest1 2
-itest1 5
-itest1 10
+# # Isolation Test
+# tlimit
+# itest1 2
+# itest1 5
+# itest1 10
+# itest2 2
+# itest2 5
+# itest2 10
 
 # Deadlock Test
 
 echo 'Final Score: '$final_score
 
-mkdir ${folder}-grade/
-mv *.log ${folder}-grade/
-mv result.txt ${folder}-grade/
+# Copy log files
+grade_folder=${group_folder}'-grade/'
+mkdir $grade_folder
+mkdir $grade_folder/atomicity/
+mv ../a*.log $grade_folder/atomicity/
+mkdir $grade_folder/consistency/
+mv ../c*.log $grade_folder/consistency/
+mkdir $grade_folder/isolation/
+mkdir $grade_folder/isolation/test1/
+mv ../i1*.log $grade_folder/isolation/test1/
+mkdir $grade_folder/isolation/test2/
+mv ../i2*.log $grade_folder/isolation/test2/
+mkdir $grade_folder/isolation/test3/
+mv ../i3*.log $grade_folder/isolation/test3/
+mkdir $grade_folder/deadlock/
+mv ../d*.log $grade_folder/deadlock/
+mv ../result.txt $grade_folder
 
